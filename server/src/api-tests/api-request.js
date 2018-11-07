f827049af8a2073341164fc6642b9721307d3ff7
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-07 23:31:50 +0200

    Fix test linter errors
    * Added eslint-plugin-mocha

--- server/src/api-tests/api-request.js
+++ server/src/api-tests/api-request.js
@@ -2,7 +2,7 @@ import request from 'request-promise-native'
 
 export default function apiRequest(path, extraOpts) {
   const opts = {
-    uri: 'http://localhost:8080' + path,
+    uri: `http://localhost:8080${path}`,
     json: true,
     ...extraOpts
   }
--- server/src/api-tests/version.test.js
+++ server/src/api-tests/version.test.js
@@ -4,8 +4,8 @@ import apiRequest from './api-request'
 describe('/api/version', () => {
   it('should return Git revision information', async () => {
     const version = await apiRequest('/api/version')
-    assert.equal(typeof(version.gitHash), 'string')
-    assert.equal(typeof(version.gitBranch), 'string')
+    assert.equal(typeof version.gitHash, 'string')
+    assert.equal(typeof version.gitBranch, 'string')
   })
 })
 
