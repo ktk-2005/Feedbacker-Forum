f6303a00201fcd7ec8967be010b42b1fa594000c
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-07 23:55:56 +0200

    Clean-up api-request.js

--- server/src/api-tests/api-request.js
+++ server/src/api-tests/api-request.js
@@ -3,11 +3,13 @@ import util from 'util'
 
 const setTimeoutPromise = util.promisify(setTimeout)
 
+// Attempt to send a request to the API server, called again
+// with a timeout if it fails to respond.
 function tryRequest(opts, retriesLeft) {
   let promise = request(opts)
 
   if (retriesLeft > 1) {
-    promise = promise.catch((error) => {
+    promise = promise.catch(() => {
       const time = 3
       return setTimeoutPromise(time * 1000)
         .then(() => tryRequest(opts, retriesLeft - 1))
@@ -17,6 +19,8 @@ function tryRequest(opts, retriesLeft) {
   return promise
 }
 
+// Perform a request to API endpoint `path`, `extraOpts` are passed as-is
+// to the `request` package.
 export default function apiRequest(path, extraOpts) {
   const opts = {
     uri: `http://localhost:8080${path}`,
