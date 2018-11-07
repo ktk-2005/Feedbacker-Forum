a9dcfeed41cdc428c132c45d67747b0489ec8f1c
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-07 23:49:06 +0200

    Test that Git hash is hexadecimal

--- server/src/api-tests/version.test.js
+++ server/src/api-tests/version.test.js
@@ -6,6 +6,7 @@ describe('/api/version', () => {
     const version = await apiRequest('/api/version')
     assert.equal(typeof version.gitHash, 'string')
     assert.equal(typeof version.gitBranch, 'string')
+    assert(version.gitHash.match(/[a-f0-9]*/))
   })
 })
 
