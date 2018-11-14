6059c922109493a1fa92bee3404f54ab015fb970
Sofia Tengvall <laura.tengvall@aalto.fi>
2018-11-14 20:03:42 +0200

    Added index.css, added button and panel to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styles from './index.css'
+import classNames from 'classnames/bind'
+
+const css = classNames.bind(styles)
 
 class VersionInfo extends React.Component {
   constructor() {
@@ -23,8 +27,59 @@ class VersionInfo extends React.Component {
   }
 }
 
+class MainView extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      panelIsVisible: false,
+      buttonIsVisible: true,
+    }
+  }
+
+  handleClick() {
+    this.setState({
+      buttonIsVisible: !this.state.buttonIsVisible,
+      panelIsVisible: !this.state.panelIsVisible,
+    })
+  }
+
+  render() {
+    return(
+      <div className="test">
+            <Button visible={this.state.buttonIsVisible}
+              onClick={this.handleClick.bind(this)}
+            />
+            <FloatingPanel visible={this.state.panelIsVisible}
+              onClick={this.handleClick.bind(this)}
+            />
+      </div>
+    )
+  }
+}
+
+const Button = ({visible, onClick}) => {
+  return (
+    <button
+      className={visible ? css('button') : css('hidden')}
+      onClick={onClick}
+    >+</button>
+  )
+}
+
+const FloatingPanel = ({visible, onClick}) => {
+    return (
+      <div className={visible ? css('panel') : css('hidden')}>
+        <button
+          className={css('panelButton')}
+          onClick={onClick}
+        >x</button>
+      </div>
+    )
+}
+
+
 ReactDOM.render(
-  <VersionInfo />,
+  <MainView />,
   document.getElementById('root')
 )
-
