80ee28e73202fa4aec02fb54a9bbeb53b8c66043
Tobias Jern <tobias.jern@aalto.fi>
2018-11-20 16:54:42 +0200

    Structured basic sqlite functionality

--- server/src/database.js
+++ server/src/database.js
@@ -1,53 +1,8 @@
-import sqlite3 from 'sqlite3'
-import path from 'path'
-import fs from 'fs'
+import SQLiteDatabase from './database/database-sqlite'
 
-// import config from './default-config'
+const db = new SQLiteDatabase()
 
-const sqlite = sqlite3.verbose()
+export async function getAllComments() { return db.query('SELECT time, text, user, url FROM comments') }
 
-export async function startDatabase(dbFile) {
-  const databaseFile = path.resolve(__dirname, dbFile || '../data/dev_db.sqlite')
+export async function addComment(values = []) { return db.run('INSERT INTO comments VALUES ((?), (?), (?), (?), (?), (?))', values) }
 
-  if (fs.existsSync(databaseFile)) {
-    return new sqlite.Database(databaseFile)
-  }
-  fs.closeSync(fs.openSync(databaseFile, 'w'))
-  const database = new sqlite.Database(databaseFile)
-
-  const migrationFile = path.resolve(__dirname, '../data/migration.sql')
-  const migration = fs.readSync(migrationFile).toString()
-  console.log(migration)
-  database.run(migration, (err, res) => console.log('migration failed', err, res))
-  return database
-}
-
-function run(db, ...args) {
-  return new Promise((resolve, reject) => {
-    db.run(...args, (err, res) => {
-      if (err) {
-        console.error(`err: ${err}`)
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  })
-}
-
-function query(db, ...args) {
-  return new Promise((resolve, reject) => {
-    db.all(...args, (err, res) => {
-      if (err) {
-        console.error(`err: ${err}`)
-        reject(err)
-      } else {
-        resolve(res)
-      }
-    })
-  })
-}
-
-export function getAllComments(db) { query(db, 'SELECT commentText, name FROM comments') }
-
-export function addComment(db, values = []) { run(db, 'INSERT INTO comments VALUES ((?), (?), (?))', values) }
---
+++ server/src/database/database-sqlite.js
@@ -0,0 +1,58 @@
+import sqlite3 from 'sqlite3'
+import path from 'path'
+import fs from 'fs'
+
+const sqlite = sqlite3.verbose()
+
+class SQLiteDatabase {
+  constructor(dbFile) {
+    this.databaseFile = path.resolve(__dirname, dbFile || '../../data/dev_db.sqlite')
+
+    /* if (fs.existsSync(this.databaseFile)) {
+      this.db = new sqlite.Database(this.databaseFile)
+    } else {
+      fs.closeSync(fs.openSync(this.databaseFile, 'w'))
+      this.db = new sqlite.Database(this.databaseFile)
+    } */
+    // console.log(this.databaseFile)
+    this.db = new sqlite.Database(this.databaseFile)
+    // console.log(this.db)
+    // console.log(this.db.all('SELECT * FROM comments'))
+    const migrationFile = path.resolve(__dirname, '../../data/migration.sql')
+    const migration = fs.readFileSync(migrationFile).toString()
+    this.db.run(migration, (err) =>  {
+      if (err) {
+        console.log(`migration failed ${err}`)
+      }
+    })
+  }
+
+  async run(...args) {
+    return new Promise((resolve, reject) => {
+      this.db.run(...args, (err, res) => {
+        if (err) {
+          console.error(`ERRR: ${err}`)
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
+  }
+
+  async query(...args) {
+    return new Promise((resolve, reject) => {
+      this.db.all(...args, (err, res) => {
+        if (err) {
+          console.error(`ERRR: ${err}`)
+          reject(err)
+        } else {
+          console.log('SUCCESS')
+          resolve(res)
+        }
+      })
+    })
+  }
+}
+
+export default SQLiteDatabase
