a9f99c356dcbff848b8e3e367c62a029ae4dc9a4
KalleMakela <kallemakela@github.com>
2018-11-20 18:27:38 +0200

    add db.js

---
+++ server/src/database.js
@@ -0,0 +1,7 @@
+import SQLiteDatabase from './database/database-sqlite'
+
+const db = new SQLiteDatabase()
+
+export async function getAllComments() { return db.query('SELECT time, text, user, url FROM comments') }
+
+export async function addComment(values = []) { return db.run('INSERT INTO comments VALUES ((?), (?), (?), (?), (?), (?))', values) }
