8d7b766b6bfd948b0762dff5d85b9491d9974311
Tobias Jern <tobias.jern@aalto.fi>
2018-11-20 16:56:50 +0200

    Cleanup

--- server/src/database/database-sqlite.js
+++ server/src/database/database-sqlite.js
@@ -9,6 +9,7 @@ class SQLiteDatabase {
     this.databaseFile = path.resolve(__dirname, dbFile || '../../data/dev_db.sqlite')
 
 <<<<<<< HEAD
+<<<<<<< HEAD
     this.db = new sqlite.Database(this.databaseFile)
 =======
     /* if (fs.existsSync(this.databaseFile)) {
@@ -22,6 +23,9 @@ class SQLiteDatabase {
     // console.log(this.db)
     // console.log(this.db.all('SELECT * FROM comments'))
 >>>>>>> Structured basic sqlite functionality
+=======
+    this.db = new sqlite.Database(this.databaseFile)
+>>>>>>> Cleanup
     const migrationFile = path.resolve(__dirname, '../../data/migration.sql')
     const migration = fs.readFileSync(migrationFile).toString()
     this.db.run(migration, (err) =>  {
