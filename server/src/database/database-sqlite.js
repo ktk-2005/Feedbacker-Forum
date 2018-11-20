e0886c8c76ac6f982423c0dcbf443b99c4fd2292
KalleMakela <kallemakela@github.com>
2018-11-20 18:26:21 +0200

    sqlite migration

---
+++ server/src/database/database-sqlite.js
@@ -0,0 +1,49 @@
+import sqlite3 from 'sqlite3'
+import path from 'path'
+import fs from 'fs'
+
+const sqlite = sqlite3.verbose()
+
+class SQLiteDatabase {
+  constructor(dbFile) {
+    this.databaseFile = path.resolve(__dirname, dbFile || '../../data/dev_db.sqlite')
+
+    this.db = new sqlite.Database(this.databaseFile)
+    const migrationFile = path.resolve(__dirname, '../../data/migration.sql')
+    const migration = fs.readFileSync(migrationFile).toString()
+    this.db.run(migration, (err) =>  {
+      if (err) {
+        console.log(`migration failed ${err}`)
+      }
+    })
+  }
+
+  async run(...args) {
+    return new Promise((resolve, reject) => {
+      this.db.run(...args, (err, res) => {
+        if (err) {
+          console.error(`ERRR: ${err}`)
+          reject(err)
+        } else {
+          resolve(res)
+        }
+      })
+    })
+  }
+
+  async query(...args) {
+    return new Promise((resolve, reject) => {
+      this.db.all(...args, (err, res) => {
+        if (err) {
+          console.error(`ERRR: ${err}`)
+          reject(err)
+        } else {
+          console.log('SUCCESS')
+          resolve(res)
+        }
+      })
+    })
+  }
+}
+
+export default SQLiteDatabase
