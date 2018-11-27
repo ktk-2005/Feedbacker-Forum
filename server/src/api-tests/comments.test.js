fa40ede99789f1ba6124b8361b3cee832046a371
Shahruz Mannan <shahruz.mannan@aalto.fi>
2018-11-27 16:59:04 +0200

    a

--- server/src/api-tests/comments.test.js
+++ server/src/api-tests/comments.test.js
@@ -1,14 +1,30 @@
 import assert from 'assert'
 import apiRequest from './api-request'
 
-
+describe('/api/comment', () => {
+  it('should return 👌 for posting a commment', async () => {
+    const emoji = await apiRequest('/api/comment', {
+      method: 'POST',
+      body: {
+	"text": "testing",
+	"user": "oseppala",
+	"blob": "{\"path\": \"/path/to/element\"}"
+}
+    })
+    //
+    assert.equal(emoji, '👌')
+    //
+    })
+})
 
 describe('/api/comments', () => {
   it('should return all comments', async () => {
-    const comments = await apiRequest('/api/comments')
+    const comments = await apiRequest('/api/comments', {
+      method: 'GET',
+    })
     //
     assert.equal(commnts[0].text, 'skrattia')
     //
-    assert(version.gitHash.match(/[a-f0-9]*/))
+      assert(version.gitHash.match(/[a-f0-9]*/))
   })
 })
