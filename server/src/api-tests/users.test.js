cd16e88e9408ed3b02d686d17ed473b702149ecc
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-27 22:42:54 +0200

    No need for body in /api/users

--- server/src/api-tests/users.test.js
+++ server/src/api-tests/users.test.js
@@ -5,10 +5,6 @@ describe('/api/users', () => {
   it('should return key and secret as strings', async () => {
     const stuff = await apiRequest('/api/users', {
       method: 'POST',
-      body: {
-        key: '65e086ce',
-        secret: '8a38d7dd343843fcbf4d136063ea6d',
-      },
     })
     assert.equal(typeof stuff.id, 'string')
     assert.equal(typeof stuff.secret, 'string')
