3b8bad80bb674d20b9671d69b1dfbfb6a2b9f017
Tobias Jern <tobias.jern@aalto.fi>
2018-11-27 14:09:59 +0200

    Separated test data from setup migration

---
+++ server/src/database/migrations/02-test-data.sql
@@ -0,0 +1,12 @@
+-- Test data for developement purposes
+
+INSERT INTO comments (id, time, text, user, url, thread_id) VALUES ('1bd8052b', '2018-11-14 16:35:27', 'skrattia', 'jaba', NULL, '55c132a6');
+INSERT INTO comments (id, time, text, user, url, thread_id) VALUES ('cb38e8f6', '2018-11-14 17:10:42', 'tröttistä', 'jaba', NULL, '8ed4ee5d');
+
+INSERT INTO questions (id, time, text, user, url, thread_id) VALUES ('eb1de3fc', '2018-11-14 17:40:09', 'Mitä tänään syötäisiin?', 'jaba', NULL, 'b07ed81a');
+
+INSERT INTO reactions (id, time, emoji, user, comment_id) VALUES ('5eaafea3', '2018-11-14 16:38:41', '👌', 'jaba', '1bd8052b');
+INSERT INTO reactions (id, time, emoji, user, comment_id) VALUES ('fb05d1a5', '2018-11-14 16:59:32', '🅱️', 'jaba', '1bd8052b');
+INSERT INTO reactions (id, time, emoji, user, comment_id) VALUES ('cba06aee', '2018-11-14 23:20:11', '🅱️', 'jaba', '1bd8052b');
+
+INSERT INTO comments (id, text, user, thread_id) VALUES ('13adr8sa', 'jaahaa', 'jaba', 'a688ad4f');
--- server/src/database/migrations/02-test.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO comments (id, text, user, thread_id) VALUES ('13adr8sa', 'jaahaa', 'jaba', 'a688ad4f');
