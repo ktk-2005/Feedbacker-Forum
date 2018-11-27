8285c296681b332627627001df6aec94b8097329
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-28 00:01:04 +0200

    Test spamming /api/users

--- server/src/api-tests/users.test.js
+++ server/src/api-tests/users.test.js
@@ -2,6 +2,13 @@ import assert from 'assert'
 import apiRequest from './api-request'
 
 describe('/api/users', () => {
+
+  it('should handle multiple posts', async () => {
+    await apiRequest('/api/users', { method: 'POST' })
+    await apiRequest('/api/users', { method: 'POST' })
+    await apiRequest('/api/users', { method: 'POST' })
+  })
+
   it('should return key and secret as strings', async () => {
     const stuff = await apiRequest('/api/users', {
       method: 'POST',
