e68c40e91cb9ed27bd28c5ba829ad0d09b5f5e48
KalleMakela <kallemakela@github.com>
2018-11-27 20:24:26 +0200

    add documentation

--- server/src/routes/comments.js
+++ server/src/routes/comments.js
@@ -9,12 +9,26 @@ import { catchErrors } from '../handlers'
 
 const router = express.Router()
 
+// @api GET /api/comments
+// Retrieve all comments.
+//
+// returns JSON array of all comments in database
 router.get('/comments', catchErrors(async (req, res) => {
   await getComments().then((rows) => {
     res.send(rows)
   })
 }))
 
+// @api POST /api/comment
+// Adds comment to database.
+//
+// @params @json {
+//   "text": "minttua",
+//   "user": "salaattipoika",
+//   "blob": "{\"path\": \"/path/to/element\"}"
+// }
+//
+// @returns 'OK' if comment is succesfully added
 router.post('/comment', catchErrors(async (req, res) => {
   const { text, user, blob } = req.body
   const id = uuid()
@@ -23,6 +37,10 @@ router.post('/comment', catchErrors(async (req, res) => {
   res.send('OK')
 }))
 
+// @api GET /api/comments/:threadId
+// Get comments by threadId
+//
+// returns JSON array of all comments in thread
 router.get('/comments/:threadId', catchErrors(async (req, res) => {
   const { threadId } = req.params
   await getThreadComments(threadId).then((rows) => {
@@ -30,12 +48,26 @@ router.get('/comments/:threadId', catchErrors(async (req, res) => {
   })
 }))
 
+// @api GET /api/questions
+// Retrieve all questions.
+//
+// returns JSON array of all questions in database
 router.get('/questions', catchErrors(async (req, res) => {
   await getQuestions().then((rows) => {
     res.send(rows)
   })
 }))
 
+// @api POST /api/question
+// adds question to database.
+//
+// @params @json {
+//   "text": "What?",
+//   "user": "salaattipoika",
+//   "blob": "{\"path\": \"/path/to/element\"}"
+// }
+//
+// @returns 'OK' if question is succesfully added
 router.post('/question', catchErrors(async (req, res) => {
   const { text, user, blob } = req.body
   const id = uuid()
@@ -44,12 +76,20 @@ router.post('/question', catchErrors(async (req, res) => {
   res.send('OK')
 }))
 
+// @api GET /api/reactions
+// Retrieve all reactions.
+//
+// returns JSON array of all reactions in database
 router.get('/reactions', catchErrors(async (req, res) => {
   await getReactions().then((rows) => {
     res.send(rows)
   })
 }))
 
+// @api GET /api/reactions/:commentId
+// Retrieve all reactions by commentId.
+//
+// returns JSON array of all reactions to comment
 router.get('/reactions/:commentId', catchErrors(async (req, res) => {
   const { commentId } = req.params
   await getCommentReactions(commentId).then((rows) => {
@@ -57,6 +97,16 @@ router.get('/reactions/:commentId', catchErrors(async (req, res) => {
   })
 }))
 
+// @api POST /api/reaction
+// add reaction to the database.
+//
+// @params @json {
+//   "emoji": "🍑",
+//   "user": "jaba",
+//   "comment_id": "1bd8052b"
+// }
+//
+// @returns 'OK' if reaction is succesfully added
 router.post('/reaction', catchErrors(async (req, res) => {
   const { emoji, user, commentId } = req.body
   const id = uuid()
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -5,6 +5,18 @@ import { catchErrors } from '../handlers'
 
 const router = express.Router()
 
+// @api POST /api/users
+// Add user to database.
+// Returns JSON that contains generated id and secret of added user.
+//
+// @params @json {
+//   "name": "salaattipoika"
+// }
+//
+// Example response @json {
+//     "id": "d6ac55e9",
+//     "secret": "ea2ca2565f484906bfd5096126816a"
+// }
 router.post('/', catchErrors(async (req, res) => {
   const { name } = req.body
   const id = uuid()
@@ -17,7 +29,10 @@ router.post('/', catchErrors(async (req, res) => {
   })
 }))
 
-
+// @api GET /api/users
+// Retrieve all users.
+//
+// returns JSON array of all users in database
 router.get('/', catchErrors(async (req, res) => {
   await getUsers().then((rows) => {
     res.send(rows)
