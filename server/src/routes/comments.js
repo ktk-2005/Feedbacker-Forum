71c5cc3c7f8556ee5e489304a87a1f5afbf34f4e
Tobias Jern <tobias.jern@aalto.fi>
2018-11-27 20:30:27 +0200

    Changed databasecall functions parameter style

--- server/src/routes/comments.js
+++ server/src/routes/comments.js
@@ -19,7 +19,9 @@ router.post('/comment', catchErrors(async (req, res) => {
   const { text, user, blob } = req.body
   const id = uuid()
   const threadId = req.body.threadId || uuid()
-  await addComment([id, text, user, threadId, blob])
+  await addComment({
+    id, text, user, threadId, blob,
+  })
   res.send('OK')
 }))
 
@@ -40,7 +42,9 @@ router.post('/question', catchErrors(async (req, res) => {
   const { text, user, blob } = req.body
   const id = uuid()
   const threadId = req.body.threadId || uuid()
-  await addQuestion([id, text, user, threadId, blob])
+  await addQuestion({
+    id, text, user, threadId, blob,
+  })
   res.send('OK')
 }))
 
@@ -60,7 +64,9 @@ router.get('/reactions/:commentId', catchErrors(async (req, res) => {
 router.post('/reaction', catchErrors(async (req, res) => {
   const { emoji, user, commentId } = req.body
   const id = uuid()
-  await addReaction([id, emoji, user, commentId])
+  await addReaction({
+    id, emoji, user, commentId,
+  })
   res.send('OK')
 }))
 
