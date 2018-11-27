02e08251601f80dbab7015cdf0897ff7fb8313e1
bqqbarbhg <samuli.1995@hotmail.com>
2018-11-27 21:34:26 +0200

    Add NOT NULL constraints

--- server/src/database/migrations/001-setup.sql
+++ server/src/database/migrations/001-setup.sql
@@ -4,46 +4,46 @@ BEGIN TRANSACTION;
 
 -- Table: users
 CREATE TABLE users (
-    id         CHAR(8) UNIQUE,
-    time       VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP), -- Postgres CURRENT_TIMESTAMP is 29 chars long
+    id         CHAR(8) UNIQUE NOT NULL,
+    time       VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP) NOT NULL, -- Postgres CURRENT_TIMESTAMP is 29 chars long
     name       VARCHAR(255),
-    secret     CHAR(30),
+    secret     CHAR(30) NOT NULL,
     blob       TEXT
 );
 
 -- Table: comments
 CREATE TABLE comments (
-    id        CHAR(8)   UNIQUE,
-    time      VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP),
-    text      TEXT,
-    user_id   CHAR(8),
-    thread_id CHAR(8),
+    id        CHAR(8) UNIQUE NOT NULL,
+    time      VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+    text      TEXT NOT NULL,
+    user_id   CHAR(8) NOT NULL,
+    thread_id CHAR(8) NOT NULL,
     blob      TEXT
 );
 
 -- Table: questions
 CREATE TABLE questions (
-    id        CHAR(8) UNIQUE,
-    time      VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP),
-    text      TEXT,
-    user_id   CHAR(8),
-    thread_id VARCHAR(8),
+    id        CHAR(8) UNIQUE NOT NULL,
+    time      VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+    text      TEXT NOT NULL,
+    user_id   CHAR(8) NOT NULL,
+    thread_id VARCHAR(8) NOT NULL,
     blob      TEXT
 );
 
 -- Table: reactions
 CREATE TABLE reactions (
-    id         CHAR(8) UNIQUE,
-    time       VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP),
-    emoji      VARCHAR(32),
-    user_id    CHAR(8),
-    comment_id CHAR(8)
+    id         CHAR(8) UNIQUE NOT NULL,
+    time       VARCHAR(30) DEFAULT (CURRENT_TIMESTAMP) NOT NULL,
+    emoji      VARCHAR(32) NOT NULL,
+    user_id    CHAR(8) NOT NULL,
+    comment_id CHAR(8) NOT NULL
 );
 
 -- Table: threads
 CREATE TABLE threads (
-  id            CHAR(8) UNIQUE,
-  container_id  INTEGER,
+  id            CHAR(8) UNIQUE NOT NULL,
+  container_id  INTEGER NOT NULL,
   blob          TEXT
 );
 
