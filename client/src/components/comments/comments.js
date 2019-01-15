c6ecef2b2890f1aa33a4e23fa72f59306fb02f73
Joona Lillfors <joona.lillfors@aalto.fi>
2019-01-15 11:07:59 +0200

    Isolated comments to own component and added reactions to comments

---
+++ client/src/components/comments/comments.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import * as R from 'ramda'
+import Reactions from '../emoji-reactions/emoji-reactions'
+
+const Comments = (data, css) => {
+  console.log(data)
+  return R.map(([id, comment]) => (
+    <div className={css('comment')} key={id}>
+      <div className={css('comment-text')}> {comment.text} </div>
+      <div className={css('comment-time')}> {comment.time} </div>
+      <Reactions reactions={comment.reactions} comment_id={id} />
+    </div>
+  ), R.toPairs(data))
+}
+
+export default Comments
