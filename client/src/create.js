6651d3c387cbca9e71b92644bc50ad93bf2da978
Elias Benkhodja <elias.benkhodja@gmail.com>
2019-01-16 12:46:57 +0200

    Update instance page to match api and add api documentation

--- client/src/create.js
+++ client/src/create.js
@@ -8,18 +8,18 @@ const Create = () => (
   <div className={css('create-view')}>
     <h2>Create an instance</h2>
     <form
-      action="/container"
+      action="/api/instances/new"
       method="post"
       className={css('form-create')}
     >
       Git URL
-      <input type="text" name="name" />
+      <input type="text" name="instance_image" />
       Git Hash
-      <input type="text" name="name" />
+      <input type="text" name="instance_version" />
       Type
-      <input type="text" name="name" />
+      <input type="text" name="instance_type" />
       Name
-      <input type="text" name="name" />
+      <input type="text" name="instance_name" />
       <input type="submit" value="Create" />
     </form>
   </div>
--- server/src/routes/instances.js
+++ server/src/routes/instances.js
@@ -7,7 +7,9 @@ const router = express.Router()
 
 
 // @api GET /api/containers
-// TODO
+// Retrieve all instances in the database.
+//
+// Returns 200 OK and a JSON array of all instances or 500 ISE if an error occurred.
 
 router.get('/', async (req, res) => {
   try {
@@ -18,6 +20,18 @@ router.get('/', async (req, res) => {
     res.sendStatus(500)
   }
 })
+
+// @api POST /api/containers/new
+// Create a new container.
+//
+// Currently the only parameter considered is `instance_image`. The name and subdomain are
+// generated automatically.
+//
+// Example body @json {
+//  "instance_image": "nginx"
+// }
+//
+// Returns 200 OK if the operation completed successfully and 500 ISE if an error occurred.
 
 router.post('/new', async (req, res) => {
   try {
@@ -30,6 +44,14 @@ router.post('/new', async (req, res) => {
   }
 })
 
+// @api POST /api/containers/stop
+// Stop a running container.
+//
+// Example body @json {
+//  "id": "212ef098098a098b0980c980980"
+// }
+//
+// Returns 200 OK if the operation completed successfully and 500 ISE if an error occurred.
 router.post('/stop', async (req, res) => {
   try {
     await stopContainer(req.body.instance_id)
@@ -40,7 +62,14 @@ router.post('/stop', async (req, res) => {
   }
 })
 
-
+// @api POST /api/containers/delete
+// Delete a container
+//
+// Example body @json {
+//  "id": "212ef098098a098b0980c980980"
+// }
+//
+// Returns 200 OK if the operation completed successfully and 500 ISE if an error occurred.
 router.post('/delete', async (req, res) => {
   try {
     await deleteContainer(req.body.intance_id)
