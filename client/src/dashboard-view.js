d588f7b466a60db562e6d9ffad67b232a576164d
Sofia Tengvall <sofia.tengvall@wippies.fi>
2019-01-16 21:23:54 +0200

    fetching instances

--- client/src/dashboard-view.js
+++ client/src/dashboard-view.js
@@ -4,19 +4,44 @@ import styles from './scss/views/dashboard-view.scss'
 
 const css = classNames.bind(styles)
 
-const Dashboard = () => (
-  <div className={css('dashboard')}>
-    <div className={css('top')}>
-      <button
-        className={css('create-button')}
-        type="button"
-      >Create new
-      </button>
-    </div>
-    <div>
-      <ul />
-    </div>
-  </div>
-)
+class Dashboard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      instances: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/instances')
+      .then(response => response.json())
+      .then(instances => this.setState({ instances }))
+  }
+
+  render () {
+    const { instances } = this.state
+
+    return (
+      <div className={css('dashboard')}>
+        <div className={css('top')}>
+          <button
+            className={css('create-button')}
+            type="button"
+          >Create new
+          </button>
+        </div>
+        <div className={css('instance-container')}>
+          <ul>
+          <h2>your containers</h2>
+            {instances.map(instance =>
+              <li key={instance.id}>{instance.id}</li>
+            )}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Dashboard
