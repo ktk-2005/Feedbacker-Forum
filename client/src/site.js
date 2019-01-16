4db2aa2e821f21bf201e38ac87f2b4e9db242d70
Sofia Tengvall <sofia.tengvall@wippies.fi>
2019-01-16 17:07:46 +0200

    Modified site.js

--- client/src/site.js
+++ client/src/site.js
@@ -10,35 +10,6 @@ import ReactRouter from './reactrouter'
 
 const css = classNames.bind(styles)
 
-const LOAD_PERSIST = 'LOAD_PERSIST'
-const SET_PERSIST = 'SET_PERSIST'
-
-function persistReducer(state = { }, action) {
-  switch (action.type) {
-    case LOAD_PERSIST:
-      return action.state
-
-    case SET_PERSIST:
-      return R.mergeDeepRight(state, action.data)
-
-    default:
-      return state
-  }
-}
-
-const reducer = combineReducers({
-  persist: persistReducer,
-})
-
-const store = createStore(reducer)
-
-const feedbackAppRoot = () => {
-  const feedbackAppRoot = document.createElement('div')
-  feedbackAppRoot.setAttribute('data-feedback-app-root', true)
-  document.body.appendChild(feedbackAppRoot)
-  return feedbackAppRoot
-}
-
 const initializedKey = '!!feedbacker_forum_initialized!!'
 
 const initialize = () => {
@@ -48,43 +19,11 @@ const initialize = () => {
 
   window[initializedKey] = true
 
-  const loadPersist = async (state, allDataLoaded) => {
-    store.dispatch({ type: LOAD_PERSIST, state })
-
-    if (allDataLoaded) {
-      if (!state.users || R.isEmpty(state.users)) {
-        const response = await fetch(`${apiUrl}/users`, {
-          method: 'POST',
-        })
-        const { id, secret } = await response.json()
-
-        console.log('Created new user from API', { [id]: secret })
-
-        store.dispatch({
-          type: SET_PERSIST,
-          data: {
-            users: {
-              [id]: secret,
-            },
-          },
-        })
-      } else {
-        console.log('Loaded user from persistent storage', state.users)
-      }
-    }
-  }
-
-  const savePersist = setupPersist(loadPersist)
-
-  store.subscribe(() => {
-    savePersist(store.getState().persist || { })
-  })
-
   ReactDOM.render(
     <div className={css('feedback-app-main-container')}>
       <ReactRouter />
     </div>,
-    feedbackAppRoot()
+    document.getElementById('root')
   )
 }
 
