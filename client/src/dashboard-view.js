4bbcbcab899a32a1da73460a851cf46483ee1c92
Shahruz Mannan <shahruz.mannan@aalto.fi>
2019-01-16 21:13:19 +0200

    site.hmtl dashboard button works

--- client/src/dashboard-view.js
+++ client/src/dashboard-view.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import styles from './scss/views/dashboard-view.scss'
+import { Route, Link } from 'react-router-dom'
+import Create from './create'
 
 const css = classNames.bind(styles)
 
 const Dashboard = () => (
   <div className={css('dashboard')}>
     <div className={css('top')}>
-      <button
-        className={css('create-button')}
-        type="button"
-      >Create new
-      </button>
+      <Link to="/site/create">
+        <button
+          className={css('create-button')}
+          type="button"
+        >Create new
+        </button>
+      </Link>
     </div>
     <div>
       <ul />
     </div>
+    <Route path="/site/create" component={Create} />
   </div>
 )
 
