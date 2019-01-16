46ba12d99a08f566fe006068bdf155b2d738dc4c
Sofia Tengvall <sofia.tengvall@wippies.fi>
2019-01-16 16:44:28 +0200

    Added site.js and testsite.html

---
+++ client/src/site.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom'
+import React from 'react'
+import ReactRouter from './reactrouter'
+
+ReactDOM.render(
+  <div>
+    <ReactRouter />
+  </div>,
+  feedbackAppRoot()
+)
