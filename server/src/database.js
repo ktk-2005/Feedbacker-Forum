84c7cdc45c4c9a828bae41fcdf62817547c9b47c
bqqbarbhg <samuli.1995@hotmail.com>
2019-01-18 01:30:31 +0200

    Remove debug log

--- server/src/database.js
+++ server/src/database.js
@@ -81,7 +81,6 @@ export async function listContainers() {
 export async function resolveContainer(subdomain) {
   const rows = await db.query('SELECT id FROM containers WHERE subdomain=? LIMIT 1', [subdomain])
   if (!rows || rows.length === 0) throw new Error(`Invalid container ${subdomain}`)
-  console.log(rows)
   return rows[0].id
 }
 
