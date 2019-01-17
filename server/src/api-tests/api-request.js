7890a640a6485c2399a4a45baad80685aa415b8b
Tobias Jern <tobias.jern@aalto.fi>
2019-01-17 18:28:45 +0200

    Added fail option to tests and tests for unique constraints

--- server/src/api-tests/api-request.js
+++ server/src/api-tests/api-request.js
@@ -2,17 +2,22 @@ import request from 'request-promise-native'
 import util from 'util'
 import errors from 'request-promise-native/errors'
 
+
 const setTimeoutPromise = util.promisify(setTimeout)
 
 let totalFailedRequests = 0
 
 // Attempt to send a request to the API server, called again
 // with a timeout if it fails to respond.
-function tryRequest(opts, retriesLeft) {
+function tryRequest(opts, retriesLeft, fail) {
   let promise = request(opts)
 
-  if (retriesLeft > 1) {
+  if (retriesLeft > 1 || fail) {
     promise = promise.catch((error) => {
+
+      if (error.statusCode === 500 && fail) {
+        return Promise.resolve('Failed')
+      }
       if (!(error instanceof errors.RequestError
         || (error instanceof errors.StatusCodeError && error.statusCode === 502))) {
         return Promise.reject(error)
@@ -27,20 +32,18 @@ function tryRequest(opts, retriesLeft) {
         .then(() => tryRequest(opts, retriesLeft - 1))
     })
   }
-
   return promise
 }
 
 // Perform a request to API endpoint `path`, `extraOpts` are passed as-is
 // to the `request` package.
-export default function apiRequest(path, extraOpts) {
+export default function apiRequest(path,extraOpts, fail = false) {
   const port = process.env.APP_SERVER_PORT || '8080'
   const opts = {
     uri: `http://localhost:${port}${path}`,
     json: true,
     ...extraOpts,
   }
-
-  return tryRequest(opts, 80)
+  return tryRequest(opts, 80 , fail)
 }
 
