90940c5dbfb89d19711fc2ce7ecbc56a31f35ed3
Tobias Jern <tobias.jern@aalto.fi>
2019-02-12 15:31:18 +0200

    Adds checkbox to comment anonymously

--- client/src/components/comment-panel/comment-panel.js
+++ client/src/components/comment-panel/comment-panel.js
@@ -50,7 +50,7 @@ class CommentPanel extends React.Component {
     this.setState({ currentThread: threadId })
   }
 
-  async handleSubmit(event, taggedElementXPath, value, threadId) {
+  async handleSubmit(event, taggedElementXPath, value, hideName, threadId) {
     event.preventDefault()
     event.nativeEvent.stopImmediatePropagation()
 
@@ -71,6 +71,7 @@ class CommentPanel extends React.Component {
 
     await apiCall('POST', '/comments', {
       text: value,
+      hideName,
       blob: getBlob(),
       threadId: threadId || null,
     })
