a5cdc037088223021427cda261dcfcc0c566145d
Joona Lillfors <joona.lillfors@aalto.fi>
2019-02-14 15:21:26 +0200

    Added fake survey to onboarding, works correctly now

--- client/src/components/open-survey-panel-button/open-survey-panel-button.js
+++ client/src/components/open-survey-panel-button/open-survey-panel-button.js
@@ -2,20 +2,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 // Helpers
 import classNames from 'classnames/bind'
-
+import * as R from 'ramda'
 // Styles
 import styles from './open-survey-panel-button.scss'
 
 const css = classNames.bind(styles)
 
-const mapStateToProps = state => ({
-  questions: state.questions,
-  role: state.role,
-})
-
+const mapStateToProps = (state) => {
+  const persist = state.persist || {}
+  return ({
+    questions: state.questions,
+    role: state.role,
+    onboarding: !R.isEmpty(persist.users) && !persist.introCompleted,
+  })
+}
 const OpenSurveyPanelButton = (props) => {
-  const { hidden, onClick, role, questions } = props
-  const disabled = role !== 'dev' && questions.length < 1
+  const { hidden, onClick, role, questions, onboarding } = props
+  const disabled = role !== 'dev' && questions.length < 1 && !onboarding
   return (
     <button
       type="button"
