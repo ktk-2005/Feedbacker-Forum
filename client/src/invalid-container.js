bd3f08a6e066b500efd9429aefd6831f67b28aae
bqqbarbhg <samuli.1995@hotmail.com>
2019-02-14 18:39:01 +0200

    Lint fix

--- client/src/invalid-container.js
+++ client/src/invalid-container.js
@@ -3,7 +3,7 @@ import React from 'react'
 function InvalidContainer({ match }) {
   const { name } = match.params
   return (
-    <h2>Feedback instance doesn't exist: {name}</h2>
+    <h2>Feedback instance doesn&apos;t exist: {name}</h2>
   )
 }
 
