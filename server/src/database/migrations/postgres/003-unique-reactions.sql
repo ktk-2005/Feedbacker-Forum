3b23ea37154ad4e7b0a1464d7b632ddd815e63bb
Kalle Makela <kalle.makela@aalto.fi>
2019-02-28 17:11:49 +0200

    fixed typo

--- server/src/database/migrations/postgres/003-unique-reactions.sql
+++ server/src/database/migrations/postgres/003-unique-reactions.sql
@@ -2,7 +2,7 @@
 BEGIN TRANSACTION;
 
 ALTER TABLE reactions
-  ADD CONSTRAINT unique_emoji_user_id_comment_id_comstraint UNIQUE(emoji, user_id, comment_id);
+  ADD CONSTRAINT unique_emoji_user_id_comment_id_constraint UNIQUE(emoji, user_id, comment_id);
 
 COMMIT TRANSACTION;
 
