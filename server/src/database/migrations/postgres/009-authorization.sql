f8644a4731ebcc18d0deef65d6d83bca844a1e12
Elias Benkhodja <elias.benkhodja@gmail.com>
2019-03-13 20:36:32 +0200

    Correct migration with wrong fk

--- server/src/database/migrations/postgres/009-authorization.sql
+++ server/src/database/migrations/postgres/009-authorization.sql
@@ -5,7 +5,7 @@ CREATE TABLE container_auth (
   subdomain  VARCHAR(32) NOT NULL,
   user_id    CHAR(8) NOT NULL,
 
-  FOREIGN KEY (subdomain) REFERENCES containers(id) ON DELETE CASCADE,
+  FOREIGN KEY (subdomain) REFERENCES containers(subdomain) ON DELETE CASCADE,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 
   UNIQUE (subdomain, user_id)
