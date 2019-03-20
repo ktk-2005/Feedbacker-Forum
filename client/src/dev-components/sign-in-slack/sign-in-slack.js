3a53e0c9fc38a9e7c082e300f0d0ba14d43103d2
Joona Lillfors <joona.lillfors@aalto.fi>
2019-03-20 17:06:53 +0200

    Fixed Sign in with Slack buttons image to be in static files instead of url to slack's site

--- client/src/dev-components/sign-in-slack/sign-in-slack.js
+++ client/src/dev-components/sign-in-slack/sign-in-slack.js
@@ -18,8 +18,6 @@ const SlackSignButton = () => (
   >
     <img
       alt="Sign in with Slack"
-      src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-      srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
     />
   </button>
 )
