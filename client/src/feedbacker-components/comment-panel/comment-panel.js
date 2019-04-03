b912d4323a12f64e2d9bddcb120b1190e361fb7a
bqqbarbhg <samuli.1995@hotmail.com>
2019-04-03 18:43:36 +0300

    Disable toast from poll

--- client/src/feedbacker-components/comment-panel/comment-panel.js
+++ client/src/feedbacker-components/comment-panel/comment-panel.js
@@ -70,7 +70,9 @@ class CommentPanel extends React.Component {
   }
 
   async fetchComments() {
-    const comments = await apiCall('GET', '/comments')
+    const comments = await apiCall('GET', '/comments', null, {
+      noToast: true,
+    })
     this.props.dispatch(loadComments(comments))
   }
 
