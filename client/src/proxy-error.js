a35975b165a48642e1596a0ecd92a5ca8300770f
Shahruz Mannan <shahruz.mannan@aalto.fi>
2019-04-05 01:48:14 +0300

    Invalid instance page styles work

--- client/src/proxy-error.js
+++ client/src/proxy-error.js
@@ -1,11 +1,17 @@
 import React from 'react'
+// Helpers
 import ReactDOM from 'react-dom'
+import classNames from  'classnames/bind'
+import { prepareReactRoot } from './shadowDomHelper'
+import InvalidInstance from './dev-components/invalid-instance/invalid-instance'
+// Styles
+import styles from './scss/_base.scss'
 
-import InvalidInstance from './dev-components/invalid-instance/invalid-instance' // './dev-components/invalid-instance/invalid-instance.js'
 
-const root = document.getElementById('root')
-
+const css = classNames.bind(styles)
 ReactDOM.render(
-  <InvalidInstance />,
-  root
+  <div className={css('feedback-app-container', 'site-views')}>
+    <InvalidInstance />
+  </div>,
+  prepareReactRoot()
 )
